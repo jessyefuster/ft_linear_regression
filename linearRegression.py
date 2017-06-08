c069a639395dff1ef795b2d50b725f1737f6d534
Jessye FUSTER <jfuster@e2r12p2.42.fr>
2017-06-08 12:44:03 +0200

    plz work

--- linearRegression.py
+++ linearRegression.py
@@ -1,18 +1,21 @@
+# import graphics
 
 def	hypothesis(inputValue, theta0 = 0.0, theta1 = 0.0):
 	"""
 	This is a linear function
 	"""
-	result = theta0 + theta1 * inputValue
+	result = theta0 + (theta1 * inputValue)
 	return (result)
+
 
 def sumCosts0(dataset, theta0, theta1):
 	costSum = 0.0
 
 	for entry in dataset:
 		costSum += hypothesis(entry[0], theta0, theta1) - entry[1]
-
+	
 	return (costSum)
+
 
 def sumCosts1(dataset, theta0, theta1):
 	costSum = 0.0
@@ -22,41 +25,37 @@ def sumCosts1(dataset, theta0, theta1):
 		
 	return (costSum)
 
+
 def trainingAlgorithm(dataset, theta0, theta1):
 	"""
 	Here we are trying to guess the optimal value of theta0 and theta1
 	according to the dataset
 	The training algorithm is a Gradient Descent
 	"""
-	learningRate = 0.3
+	learningRate = 0.0001
+	dataLen = float(len(dataset))
 
-	while (True):
-		p0 = ( learningRate * (1.0 / float(len(dataset))) * sumCosts0(dataset, theta0, theta1) )
-		tmp0 = theta0 - p0
+	while True:
+		gradient0 = learningRate * ( sumCosts0(dataset, theta0, theta1) / dataLen )
+		gradient1 = learningRate * ( sumCosts1(dataset, theta0, theta1) / dataLen )
 
-		p1 = ( learningRate * (1.0 / float(len(dataset))) * sumCosts1(dataset, theta0, theta1) )
-		tmp1 = theta1 - p1
+		theta0 -= gradient0
+		theta1 -= gradient1
 
-		# print ("Theta0 : {}   primitive: {}\nTheta1 : {}   primitive: {} ".format(theta0, theta1, ( learningRate * (1.0 / float(len(dataset))) * sumCosts0(dataset, theta0, theta1) ), ( learningRate * (1.0 / float(len(dataset))) * sumCosts1(dataset, theta0, theta1) )))
-
-		theta0 = tmp0
-		theta1 = tmp1
-
-		if abs(p0) < 0.1 and abs(p1) < 0.1:
+		if abs(gradient0) < 0.001 and abs(gradient1) < 0.001:
 			return (theta0, theta1)
+
+	return (theta0, theta1)
 
 
 if __name__ == '__main__':
-	
-	# theta0, theta1 = 0
-	# inputValue = 4500
-	# outputValue = linearFunction(inputValue)
 
 	dataset = (
 		(240000.0, 3640.0),
 		(139800.0, 3800.0),
 		(150500.0, 4400.0),
-		(185530.0, 4450.0)
+		(185530.0, 4450.0),
+		(260000.0, 3450.0)
 	)
 
 	theta0, theta1 = trainingAlgorithm(dataset, 0.0, 0.0)
