d40d37ef4dddffcb979de8e1ef89fc8c0a3420b7
Jessye FUSTER <jfuster@e1r9p8.42.fr>
2017-06-09 19:30:35 +0200

    jenaimar

--- featureScaling.py
+++ featureScaling.py
@@ -1,4 +1,31 @@
 
+def getNum0(string):
+	zeros = 0
+	for c in string:
+		if c == '0':
+			zeros += 1
+		else:
+			break
+	return (zeros)
+
+def getDecimalNumber(number):
+	res = 0
+	strNum = str(number)
+
+	print(strNum.split('.'))
+
+	# before, after = strNum.split('.')
+	
+	# if int(before) + int(after) == 0:
+	# 	pass
+	# elif int(before) > 0:
+	# 	res = len(before) - 1
+	# elif len(after) > 0:
+	# 	res = (- (len(getNum0(after))))
+
+	# print(number, res)
+
+
 def scale(dataset, key, value):
 	mean = sum(dataset[key]) / dataset['len']
 	maxX = max(dataset[key])
--- linearRegression.py
+++ linearRegression.py
@@ -1,7 +1,8 @@
 # import plotly.plotly as py
 # import plotly.graph_objs as go
 import math
-from featureScaling import scale
+import sys
+from featureScaling import scale, getDecimalNumber
 
 def	hypothesis(inputValue, theta0 = 0.0, theta1 = 0.0):
 	"""
@@ -43,8 +44,8 @@ def trainingAlgorithm(dataset, theta0, theta1):
 	according to the dataset
 	The training algorithm is a Gradient Descent
 	"""
-	learningRate0, learningRate1 = 0.001, 0.001
-	dataLen = float(len(dataset))
+	learningRate0, learningRate1 = 10., 10.
+	dataLen = float(dataset['len'])
 
 	i = 0
 
@@ -54,47 +55,48 @@ def trainingAlgorithm(dataset, theta0, theta1):
 		gradient0 = learningRate0 * sumFunction0
 		gradient1 = learningRate1 * sumFunction1
 
-
 		if i:
 			while abs(gradient0) > lastStep0:
 				learningRate0 *= 0.8
 				gradient0 = learningRate0 * sumFunction0
-				# gradient0 = learningRate0 * ( sumCosts0(dataset, theta0, theta1) / dataLen )
-				# print("rate0", learningRate0)
 
 			while abs(gradient1) > lastStep1:
 				learningRate1 *= 0.8
 				gradient1 = learningRate1 * sumFunction1
-				# gradient1 = learningRate1 * ( sumCosts1(dataset, theta0, theta1) / dataLen )
-				# print("rate1", learningRate1)
-
-		lastStep0, lastStep1 = abs(gradient0), abs(gradient1)
-		# print("loop")
+		
 		theta0 = theta0 - gradient0
 		theta1 = theta1 - gradient1
 
-		# if not math.isnan(theta0) and not math.isnan(theta1):
-		# 	print(theta0, theta1)
+		# print (theta0, theta1)
 
-		if abs(gradient0) < 0.000001 and abs(gradient1) < 0.000001:
+		# if i and abs(lastStep0 - abs(gradient0)) < 0.0000001 and abs(lastStep1 - abs(gradient1)) < 0.000001:
+			# return (theta0, theta1)
+		if math.isnan(theta0) and math.isnan(theta1):
+			print("ERROR")
+			sys.exit(1)
+
+		if abs(gradient0) < 0.0000001 and abs(gradient1) < 0.0000001:
 			return (theta0, theta1)
 		
+		lastStep0, lastStep1 = abs(gradient0), abs(gradient1)
 		i = 1
 
 
 if __name__ == '__main__':
 
+	# dataset = {
+	# 	'km':		[240000., 139800., 150500., 185530., 176000., 114800., 166800., 89000., 144500., 84000., 82029., 63060., 74000., 97500., 67000., 76025., 48235., 93000., 60949., 65674., 54000., 68500., 22899., 61789.],
+	# 	'price':	[3650., 3800., 4400., 4450., 5250., 5350., 5800., 5990., 5999., 6200., 6390., 6390., 6600., 6800., 6800., 6900., 6900., 6990., 7490., 7555., 7990., 7990., 7990., 8290.],
+	# 	'len':		24
+	# }
+
 	dataset = {
-		'km':		[240000., 139800., 150500., 185530., 176000., 114800., 166800., 89000., 144500., 84000., 82029., 63060., 74000., 97500., 67000., 76025., 48235., 93000., 60949., 65674., 54000., 68500., 22899., 61789.],
-		'price':	[3650., 3800., 4400., 4450., 5250., 5350., 5800., 5990., 5999., 6200., 6390., 6390., 6600., 6800., 6800., 6900., 6900., 6990., 7490., 7555., 7990., 7990., 7990., 8290.],
-		'len':		24
+		'km':		[1., 2., 3., 4.],
+		'price':	[3., 5., 7., 9.],
+		'len':		4
 	}
 
-	# dataset = {
-	# 	'km':		[1., 2., 3., 4.],
-	# 	'price':	[3., 5., 7., 9.],
-	# 	'len':		4
-	# }
+	# getDecimalNumber(0.00005)
 
 	# trace = go.Scatter(
 	# 	x = [1, 2, 3, 4],
@@ -109,7 +111,7 @@ if __name__ == '__main__':
 	theta0, theta1 = trainingAlgorithm(dataset, 0., 0.)
 
 	print("Theta0 : {}\nTheta1 : {}".format(theta0, theta1))
-	print("output for 42 000:")
-	print(hypothesis(420000., theta0, theta1))
+	print("output for 2:")
+	print(hypothesis(3., theta0, theta1))
 
 	# print("The output value is {}".format(outputValue))
